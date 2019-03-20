c2d6bbfaf180426f1ef120be8b4d8f9b872e5dae
PrzemekGdula <przemo.g12@wp.pl>
2019-03-20 13:15:27 +0100

    "make 100 cars"

--- script1.js
+++ script1.js
@@ -1,30 +1,17 @@
-function move (combustion) {
-    this.fuelTank = this.fuelTank - combustion
-
-    if(this.fuelTank < 0) this.fuelTank = 0
+const makeCar = function (fuelTank) {
+    return {
+        fuelTank: fuelTank,
+        move: function (combustion) {
+            this.fuelTank = this.fuelTank - combustion
+        },
+    }
 }
+const cars = []
 
-const car1 = {
-    fuelTank: 100,
-    move: move,
+for (let i = 0; i < 100; i++) {
+   const car = makeCar(100-i)
+
+   cars.push(car)
+    
 }
-
-const car2 = {
-    fuelTank:100,
-    move: move.bind(car1),
-}
-
-car1.move(10)
-car1.move(10)
-
-car2.move(10)
-car2.move(10)
-car2.move(10)
-
-console.log('car1', car1)
-
-console.log('car2', car2)
-
-
-//above line should display
-//{fuel tank; 80 } 
+console.log(cars);
