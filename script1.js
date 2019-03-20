a5bab5bf551ce8093484bcb20b0e50b6990aab6a
PrzemekGdula <przemo.g12@wp.pl>
2019-03-20 11:59:55 +0100

    "car move bind"

--- script1.js
+++ script1.js
@@ -1,5 +1,5 @@
-function move () {
-    this.fuelTank = this.fuelTank - 10
+function move (combustion) {
+    this.fuelTank = this.fuelTank - combustion
 
     if(this.fuelTank < 0) this.fuelTank = 0
 }
@@ -11,17 +11,19 @@ const car1 = {
 
 const car2 = {
     fuelTank:100,
-    move: move
+    move: move.bind(car1),
 }
 
-car1.move()
-car1.move()
+car1.move(10)
+car1.move(10)
 
-car2.move()
+car2.move(10)
+car2.move(10)
+car2.move(10)
 
-console.log(car1)
+console.log('car1', car1)
 
-console.log(car2)
+console.log('car2', car2)
 
 
 //above line should display
