a27ab3373afd7880ac4ee3de776ba43232b92951
PrzemekGdula <przemo.g12@wp.pl>
2019-03-21 08:19:30 +0100

    "counter with + button"

--- script1.js
+++ script1.js
@@ -1,3 +1,31 @@
 function Counter () {
     this.number = 0
+
+    this.render()
+}
+
+Counter.prototype.inc = function () {
+    this.number = this.number + 1
+
+    console.log(this)
+
+    this.render()
+}
+
+Counter.prototype.render = function () {
+    document.body.innerHTML = ''
+
+    const h1 = document.createElement('h1')
+    const button = document.createElement('button')
+
+    h1.innerText = this.number
+    button.innerText  = '+'
+
+    button.addEventListener(
+        'click',
+        this.inc.bind(this)
+    )
+
+    document.body.appendChild(h1)
+    document.body.appendChild(button)
 }
