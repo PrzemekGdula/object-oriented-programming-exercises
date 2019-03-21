faaaa311f6871ead64ed679bd56916b489409ab6
PrzemekGdula <przemo.g12@wp.pl>
2019-03-21 10:05:12 +0100

    task3 powered , square

--- script2.js
+++ script2.js
@@ -1,11 +1,22 @@
-function Sum (a, b) {
+function Square (x) {
 
-
-    this.sum = a + b
+    this.square = x * x
 }
 
-const sum1 = new Sum(1, 4)
-const sum2 = new Sum(3, 4)
+const result1 = new Square(1)
+const result2 = new Square(2)
 
 
+function Power (number, factor) {
+    let sum = 1
+
+    for(let i = 0; i < factor; i++) {
+        sum = sum * number
+    }
+    this.powered = sum
+}
+
+const result3 = new Power(2, 1)
+const result4 = new Power(2, 2)
+const result5 = new Power(2, 4)
 
